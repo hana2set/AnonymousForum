bf7529d8ac02bf2057412e8c3358e4c946d72275
DESKTOP-JQHEUAO\User <devkim4251@gmail.com>
2023-12-04 11:48:52 +0900

    test: comment 수정 service 테스트 추가

---
+++ src/test/java/com/study/todocard/service/CommentServiceTest.java
@@ -0,0 +1,93 @@
+package com.study.todocard.service;
+
+import com.study.todocard.dto.CommentRequestDto;
+import com.study.todocard.dto.CommentResponseDto;
+import com.study.todocard.entity.Card;
+import com.study.todocard.entity.Comment;
+import com.study.todocard.entity.User;
+import com.study.todocard.exception.BusinessException;
+import com.study.todocard.repository.CardRepository;
+import com.study.todocard.repository.CommentRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class) // @Mock 사용을 위해 설정합니다.
+class CommentServiceTest {
+
+    @Mock
+    CommentRepository commentRepository;
+
+    @Mock
+    CardRepository cardRepository;
+
+    @Test
+    @DisplayName("코멘트 수정 성공")
+    void test2() {
+        // given
+        Long CommentId = 200L;
+        CommentRequestDto requestDto = new CommentRequestDto();
+        requestDto.setContents("테스트 수정 성공");
+
+        Card card = new Card();
+
+        User user = new User();
+        user.setId(100L);
+
+        Comment comment = new Comment(requestDto, card, user);
+
+        CommentService commentService = new CommentService(commentRepository, cardRepository);
+
+        given(commentRepository.findById(CommentId)).willReturn(
+                Optional.of(comment)
+        );
+
+        // when
+        CommentResponseDto result = commentService.updateComment(CommentId, requestDto, user);
+
+        // then
+        assertEquals("테스트 수정 성공", result.getContents());
+    }
+
+    @Test
+    @DisplayName("코멘트 수정 실패 - 작성자가 아닌 유저가 코멘트 수정")
+    void test1() {
+        // given
+        Long CommentId = 200L;
+        CommentRequestDto requestDto = new CommentRequestDto();
+
+        Card card = new Card();
+
+        User user1 = new User();
+        user1.setId(100L);
+
+        Comment comment = new Comment(requestDto, card, user1);
+
+        CommentService commentService = new CommentService(commentRepository, cardRepository);
+
+        given(commentRepository.findById(CommentId)).willReturn(
+                Optional.of(comment)
+        );
+
+        User user2 = new User();
+        user1.setId(200L);
+
+        // when
+        Exception exception = assertThrows(BusinessException.class, () -> {
+            CommentResponseDto result = commentService.updateComment(CommentId, requestDto, user2);
+        });
+
+        // then
+        assertEquals("작성자만 삭제/수정할 수 있습니다", exception.getMessage());
+    }
+}
