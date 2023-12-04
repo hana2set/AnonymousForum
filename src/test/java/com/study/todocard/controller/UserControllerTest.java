b918601de2b3b00537bee6ddcebf4bbcd8cb5326
DESKTOP-JQHEUAO\User <devkim4251@gmail.com>
2023-12-04 11:48:10 +0900

    test(api/user/signup): user 컨트롤러 테스트 추가 test(api/comments/{commentId}): comment 컨트롤러 테스트 추가

---
+++ src/test/java/com/study/todocard/controller/UserControllerTest.java
@@ -0,0 +1,80 @@
+package com.study.todocard.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.todocard.entity.User;
+import com.study.todocard.entity.UserRoleEnum;
+import com.study.todocard.security.*;
+import com.study.todocard.security.dto.LoginRequestDto;
+import com.study.todocard.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.security.Principal;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@WebMvcTest(
+        controllers = {UserController.class},
+        excludeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.ASSIGNABLE_TYPE,
+                        classes = WebSecurityConfig.class
+                )
+        }
+)
+class UserControllerTest {
+    private MockMvc mvc;
+    @Autowired
+    private WebApplicationContext context;
+
+    @MockBean
+    UserService userService;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity(new MockSpringSecurityFilter()))
+                .build();
+    }
+
+    //TODO filter 로그인 테스트
+
+    @Test
+    @DisplayName("회원 가입 요청 처리")
+    void test2() throws Exception {
+        // given
+        MultiValueMap<String, String> signupRequestForm = new LinkedMultiValueMap<>();
+        signupRequestForm.add("username", "robbie");
+        signupRequestForm.add("password", "12341234");
+        signupRequestForm.add("email", "robbie@test.com");
+        signupRequestForm.add("admin", "false");
+
+        // when - then
+        mvc.perform(post("/api/user/signup")
+                        .params(signupRequestForm)
+                )
+                .andExpect(status().is2xxSuccessful())
+                .andDo(print());
+    }
+}
